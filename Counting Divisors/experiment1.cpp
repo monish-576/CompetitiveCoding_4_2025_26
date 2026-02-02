8d92cd2e4319d5bd663eb8be4aa1db64e76ee376
monish-576 <santhapurimonish7@gmail.com>
2026-02-03 01:59:45 +0530

    submission2

---
+++ Counting Divisors/experiment1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int divi(int n)
+{
+    int k=0;
+    for(int i=1;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i*i==n)
+            k++;
+            else
+            k+=2;
+        }
+    }
+    return k;
+}
+int main()
+{
+   int n;
+   cin>>n;
+   while(n>0)
+   {
+       int b;
+       cin>>b;
+       int a=divi(b);
+       cout<<a<<endl;
+       n--;
+   }
+}
